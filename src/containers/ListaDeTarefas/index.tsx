ea5652b0cbf25223a13127daa160074bd27ed587
Henriquedearaujo <ricke_cris@hotmail.com>
2024-08-13 16:33:30 -0300

    estilizando botoes e usando enums

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,7 +1,44 @@
+import Tarefa from '../../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../ultils/enums/Tarafa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Estudar redux.js',
+    descricao: 'Como preparar o anbiente ',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.CONCLUIDO
+  },
+  {
+    titulo: 'Pagar a conta de internet',
+    descricao: 'Dia 30 de setembro',
+    prioridade: enums.Prioridade.NORMAL,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
-  </main>
+    <ul>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            prioridade={t.prioridade}
+            status={t.status}
+          />
+        </li>
+      ))}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
