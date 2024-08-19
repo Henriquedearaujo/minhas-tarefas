b5218144be906c29f1ee5a86f6dc1b1304639f03
Henriquedearaujo <ricke_cris@hotmail.com>
2024-08-19 12:55:45 -0300

    filtrando as tarefas por status e prioridades

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -3,17 +3,49 @@ import { useSelector } from 'react-redux'
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 import { RootReducer } from '../../store'
+import { Prioridade } from '../../ultils/enums/Tarafa'
 
 const ListaDeTarefas = () => {
   const { itens } = useSelector((state: RootReducer) => state.tarefas)
+  const { termo, criterio, valor } = useSelector(
+    (state: RootReducer) => state.filtro
+  )
+
+  const filtraDeTarefas = () => {
+    let tarefasFiltradas = itens
+    if (termo !== undefined) {
+      tarefasFiltradas = tarefasFiltradas.filter(
+        (item) =>
+          item.titulo.toLocaleLowerCase().search(termo.toLocaleLowerCase()) >= 0
+      )
+
+      if (criterio === 'prioridade') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.prioridade === valor
+        )
+      } else if (criterio === 'status') {
+        tarefasFiltradas = tarefasFiltradas.filter(
+          (item) => item.status === valor
+        )
+      }
+      return tarefasFiltradas
+    } else {
+      return itens
+    }
+  }
 
   return (
     <Container>
       <p>
-        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;termo&ldquo;
+        2 tarefas marcadas como: &quot;categoria&ldquo; e &quot;{termo}&ldquo;
       </p>
       <ul>
-        {itens.map((t) => (
+        <li>{termo}</li>
+        <li>{criterio}</li>
+        <li>{valor}</li>
+      </ul>
+      <ul>
+        {filtraDeTarefas().map((t) => (
           <li key={t.titulo}>
             <Tarefa
               id={t.id}
